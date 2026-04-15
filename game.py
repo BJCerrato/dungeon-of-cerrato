d9aa6a4ebd9e282b8f5d1532578c25342a8e86ee
BJCerrato <brunocerrato@gmail.com>
2026-04-15 10:39:06 -0300

    Add sistema de combate por turno

--- game.py
+++ game.py
@@ -18,5 +18,40 @@ def exibir_status():
     print(f"  Pontuação: {player['pontuacao']}")
     print("=" * 30)
     
+def combate():
+    print(f"\nUm {inimigo['nome']} apareceu!")
+    print(f"HP do inimigo: {inimigo['hp']}\n")
+    
+    while inimigo["hp"] > 0 and player["hp"] > 0:
+        print("O que você faz?")
+        print("1 - Atacar")
+        print("2 - Fugir")
+        acao = input("> ")
+        
+        if acao == "1":
+            print(f"Player ataca! Inimigo perde {player['ataque']} HP")
+            inimigo["hp"] = inimigo["hp"] - player["ataque"]
+            if inimigo["hp"] > 0:
+                print(f"Inimigo ataca! Player perde {inimigo['ataque']} HP")
+                if (player["hp"] - inimigo["ataque"]) > 0:
+                    player["hp"]= player["hp"] - inimigo["ataque"]
+                    exibir_status()
+                else:
+                    player["hp"] = 0
+                    exibir_status()
+                    print("Player perdeu!")
+                    print("GAME OVER")
+            else:
+                player["ouro"] = player["ouro"] + 2
+                player["pontuacao"] = player["pontuacao"] + 50
+                print("Player venceu!")
+                exibir_status()
+        elif acao == "2":
+            print("Player fugiu!")
+            break
+        else:
+            print("Ação inválida! Selecione uma das opções disponíveis!")
+    
 if __name__=="__main__":
     exibir_status()
+    combate()
